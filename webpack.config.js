3c4e0b4e0ab9143d67c2c152ee100ecf6b261f3d
kawasakiCowboy <gmuradian@yandex.ru>
2025-01-31 12:18:13 +0300

    added a new font

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,8 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+        loader: 'url-loader?limit=100000' },
     ],
   },
 };
